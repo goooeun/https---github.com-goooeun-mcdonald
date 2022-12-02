74b52b2103860dc744994ec280f1042f637503e7
goeun <eun3903@gmail.com>
2022-12-03 05:10:45 +0900

    feat: Add get menu list from firebase

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,61 +1,66 @@
 import styled from 'styled-components';
 import theme from 'assets/style/theme';
 import { Link } from 'react-router-dom';
+import IMenu from 'types/Menu';
 
-function MenuItem() {
-    const MenuItemBlock = styled.div`
-        width: calc((100% - 88px) / 5);
-        @media screen and (max-width: 1200px) {
-            width: calc((100% - 66px) / 4);
-        }
-        border-radius: 16px;
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
-        a {
-            height: 230px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-        &:hover {
-            border: 1px solid ${theme.colors.yellow};
-        }
-        position: relative;
-        &::before {
-            content: '';
-            width: 80px;
-            height: 80px;
-            position: absolute;
-            top: 30px;
-            left: 30px;
-            border-radius: 50%;
-            background-color: ${theme.colors.lightGray};
-        }
-    `;
+const Wrapper = styled.div`
+    width: calc((100% - 88px) / 5);
+    @media screen and (max-width: 1200px) {
+        width: calc((100% - 66px) / 4);
+    }
+    border-radius: 16px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+    a {
+        height: 230px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    &:hover {
+        border: 1px solid ${theme.colors.yellow};
+    }
+    position: relative;
+    &::before {
+        content: '';
+        width: 80px;
+        height: 80px;
+        position: absolute;
+        top: 30px;
+        left: 30px;
+        border-radius: 50%;
+        background-color: ${theme.colors.lightGray};
+    }
+`;
 
-    const ImgWrapper = styled.div`
-        width: calc(100% - 32px);
-        height: 55%;
-        position: relative;
-    `;
+const ImgWrapper = styled.div`
+    width: calc(100% - 32px);
+    height: 55%;
+    position: relative;
+`;
 
-    const Img = styled.img`
-        width: 170px;
-    `;
+const Img = styled.img`
+    width: 170px;
+`;
 
+type MenuType = {
+    item: IMenu;
+};
+
+function MenuItem({ item }: MenuType) {
     return (
-        <MenuItemBlock>
-            <Link to="/menu/001">
+        <Wrapper>
+            <Link to={`/menu/${item.id}`}>
                 <ImgWrapper>
                     <Img
-                        src={`${process.env.PUBLIC_URL}/assets/burger/001.png`}
+                        src={`${process.env.PUBLIC_URL}/assets/burger/${item.img}`}
                     />
                 </ImgWrapper>
-                <h3>베이컨토마토디럭스</h3>
-                <h5>Bacon Tomato deluxe</h5>
+                <h3>{item.name}</h3>
+                <h5>{item.nameEn}</h5>
             </Link>
-        </MenuItemBlock>
+        </Wrapper>
     );
 }
 
--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -1,25 +1,37 @@
 import styled from 'styled-components';
 import MenuItem from './MenuItem';
+import { useEffect, useState } from 'react';
+import IMenu from 'types/Menu';
+import { collection, getDocs } from 'firebase/firestore';
+import database from '../../firebase';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+`;
 
 function MenuList() {
-    const MenuItems = styled.div`
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-    `;
+    const [menu, setMenu] = useState<IMenu[]>([]);
+    useEffect(() => {
+        async function getMenu() {
+            const menuCollection = collection(database, 'menu');
+            const snapshot = await getDocs(menuCollection);
+            const data: IMenu[] = snapshot.docs.map(
+                (doc) => doc.data() as IMenu
+            );
+            setMenu(data);
+        }
+        getMenu();
+    }, []);
 
     return (
-        <MenuItems>
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-        </MenuItems>
+        <Wrapper>
+            {menu &&
+                menu.map((item) => {
+                    return <MenuItem key={item.id} item={item} />;
+                })}
+        </Wrapper>
     );
 }
 
